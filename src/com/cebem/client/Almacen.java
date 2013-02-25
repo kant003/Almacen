3db7d3643a0562e7be90f8c7cbd42d20442cf283
Angel <Angel@media-PC>
2013-02-25 02:24:32 +0100

    dd

--- src/com/cebem/client/Almacen.java
+++ src/com/cebem/client/Almacen.java
@@ -17,6 +17,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 // hola
+//eoo
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
